163036701368bc61e465f2d9867451a946c00f65
lxzrvi <lxzrvi@gmail.com>
2026-08-26 23:06:39 +0530

    Update Home.kt

--- app/src/main/java/com/xmo/music/ui/Home.kt
+++ app/src/main/java/com/xmo/music/ui/Home.kt
@@ -1,1 +1,18 @@
+package com.xmo.music.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun Home() = Box(
+    Modifier.fillMaxSize().background(
+        Brush.linearGradient(
+            listOf(Color(0xFF050505), Color(0xFF321010))
+        )
+    )
+)
