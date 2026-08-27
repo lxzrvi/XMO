afec4e1527c6f1238ff4a7d993053865af8c87c5
lxzrvi <lxzrvi@gmail.com>
2026-08-27 22:16:12 +0530

    Update Artwork.kt

--- app/src/main/java/com/xmo/music/ui/Artwork.kt
+++ app/src/main/java/com/xmo/music/ui/Artwork.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.graphics.ColorUtils
 import androidx.palette.graphics.Palette
 import kotlinx.coroutines.Dispatchers
