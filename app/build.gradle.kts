a3d4d16ffbb463ec925c4b925886531001933300
lxzrvi <lxzrvi@gmail.com>
2026-08-27 21:18:08 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,10 +28,8 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.08.00"))
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.material3:material3")
-
     implementation("androidx.compose.material:material-icons-core")
-    implementation("androidx.compose.material:material-icons-extended")
-
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
     implementation("io.coil-kt.coil3:coil-compose:3.3.0")
     implementation("androidx.palette:palette-ktx:1.0.0")
 }
