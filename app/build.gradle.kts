e22f1b25e3efeedcc5fc0634f9b71eb552869b39
lxzrvi <lxzrvi@gmail.com>
2026-08-28 15:09:18 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,25 +21,65 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility =
+            JavaVersion.VERSION_17
+
+        targetCompatibility =
+            JavaVersion.VERSION_17
     }
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2025.08.00"))
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2025.08.00"
+        )
+    )
 
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(
+        "androidx.activity:activity-compose:1.10.1"
+    )
 
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-core")
+    implementation(
+        "androidx.compose.material3:material3"
+    )
 
-    implementation("androidx.datastore:datastore-preferences:1.1.7")
+    implementation(
+        "androidx.compose.material:material-icons-core"
+    )
 
-    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    implementation(
+        "androidx.datastore:datastore-preferences:1.1.7"
+    )
 
-    implementation("androidx.palette:palette-ktx:1.0.0")
+    implementation(
+        "io.coil-kt.coil3:coil-compose:3.3.0"
+    )
 
-    implementation("androidx.media3:media3-exoplayer:1.8.0")
-    implementation("androidx.media3:media3-session:1.8.0")
+    implementation(
+        "androidx.palette:palette-ktx:1.0.0"
+    )
+
+    implementation(
+        "androidx.media3:media3-exoplayer:1.8.0"
+    )
+
+    implementation(
+        "androidx.media3:media3-session:1.8.0"
+    )
+
+    /*
+     * XMO shared Liquid Glass
+     */
+    implementation(
+        "dev.chrisbanes.haze:haze:2.0.0-alpha03"
+    )
+
+    implementation(
+        "dev.chrisbanes.haze:haze-blur:2.0.0-alpha03"
+    )
+
+    implementation(
+        "dev.chrisbanes.haze:haze-blur-materials:2.0.0-alpha03"
+    )
 }
