2fc06f43d81ed90b457c124c071974394206d31d
lxzrvi <lxzrvi@gmail.com>
2026-08-28 12:41:08 +0530

    Update MiniPlayer.kt

--- app/src/main/java/com/xmo/music/ui/MiniPlayer.kt
+++ app/src/main/java/com/xmo/music/ui/MiniPlayer.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
