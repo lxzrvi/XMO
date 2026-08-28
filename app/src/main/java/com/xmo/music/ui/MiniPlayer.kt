d4886b9781fd7a947caee6079e43232ae5ae4fb4
lxzrvi <lxzrvi@gmail.com>
2026-08-28 12:10:27 +0530

    Update MiniPlayer.kt

--- app/src/main/java/com/xmo/music/ui/MiniPlayer.kt
+++ app/src/main/java/com/xmo/music/ui/MiniPlayer.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.draw.clip
