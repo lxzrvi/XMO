e447e6068f79e09461cb6af6e98394e386c532d1
lxzrvi <lxzrvi@gmail.com>
2026-08-28 19:45:17 +0530

    Update HomeParts.kt

--- app/src/main/java/com/xmo/music/ui/HomeParts.kt
+++ app/src/main/java/com/xmo/music/ui/HomeParts.kt
@@ -9,6 +9,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
