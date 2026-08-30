149e38c882f6119ab302b23fc3c5e860004204fe
lxzrvi <lxzrvi@gmail.com>
2026-08-30 21:26:13 +0530

    Update HomeThemeCompat.kt

--- app/src/main/java/com/xmo/music/ui/HomeThemeCompat.kt
+++ app/src/main/java/com/xmo/music/ui/HomeThemeCompat.kt
@@ -1,9 +1,30 @@
 package com.xmo.music.ui
 
+import androidx.annotation.DrawableRes
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import com.xmo.music.XmoTheme
-import com.xmo.music.ui.home.HomeColors
+
+typealias HomeColors = com.xmo.music.ui.home.HomeColors
 
 fun homeColors(
     theme: XmoTheme
 ): HomeColors =
     com.xmo.music.ui.home.homeColors(theme)
+
+@Composable
+fun XmoIcon(
+    @DrawableRes icon: Int,
+    tint: Color,
+    modifier: Modifier = Modifier
+) {
+    Icon(
+        painter = painterResource(icon),
+        contentDescription = null,
+        tint = tint,
+        modifier = modifier
+    )
+}
