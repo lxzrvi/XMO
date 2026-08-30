59d8a1ed98352f530c91f2b746f83422c4a1338f
lxzrvi <lxzrvi@gmail.com>
2026-08-30 21:22:54 +0530

    Create HomeThemeCompat.kt

---
+++ app/src/main/java/com/xmo/music/ui/HomeThemeCompat.kt
@@ -0,0 +1,9 @@
+package com.xmo.music.ui
+
+import com.xmo.music.XmoTheme
+import com.xmo.music.ui.home.HomeColors
+
+fun homeColors(
+    theme: XmoTheme
+): HomeColors =
+    com.xmo.music.ui.home.homeColors(theme)
