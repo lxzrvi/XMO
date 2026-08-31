0aa0b0671d41429c4fae2bbc57a6afc71dc5422f
lxzrvi <lxzrvi@gmail.com>
2026-08-31 11:06:11 +0530

    Update HomeAllSongs.kt

--- app/src/main/java/com/xmo/music/ui/home/HomeAllSongs.kt
+++ app/src/main/java/com/xmo/music/ui/home/HomeAllSongs.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
